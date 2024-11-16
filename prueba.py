29b11c3fc63976756dcdd55992cfe00fc0946a0b
petaceta79 <66634460+petaceta79@users.noreply.github.com>
2024-11-16 15:07:08 +0100

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -11,6 +11,4 @@ participants = load_participants(data_path)
 # Recorrer la lista de participantes y mostrar sus nombres
 for i, participant in enumerate(participants, start=1):
     print(f"Participante {i}: {participant.name}")
-=======
-print("soy gay")
->>>>>>> 398616571ca60c8635faa0bd8c94c9e92500f751
+    
