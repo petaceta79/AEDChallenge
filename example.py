66f7e69e4357df0be857382bdc051dd837a3c1a1
petaceta79 <66634460+petaceta79@users.noreply.github.com>
2024-11-16 15:04:48 +0100

    base

--- example.py
+++ example.py
@@ -1,8 +1,11 @@
 from rich import print
-
 from participant import load_participants
 
+# Ruta al archivo JSON
 data_path = "data/datathon_participants.json"
+
+# Cargar participantes
 participants = load_participants(data_path)
 
-print(participants[0])
+# Acceder al nombre del primer participante
+print(participants[0].name)
--- prueba.py
+++ prueba.py
@@ -1,4 +1,12 @@
 from rich import print
 from participant import load_participants
 
-print("pene")
+# Ruta al archivo JSON
+data_path = "data/datathon_participants.json"
+
+# Cargar participantes
+participants = load_participants(data_path)
+
+# Recorrer la lista de participantes y mostrar sus nombres
+for i, participant in enumerate(participants, start=1):
+    print(f"Participante {i}: {participant.name}")
