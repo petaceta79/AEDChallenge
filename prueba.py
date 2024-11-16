6e7caee66d9e036e451b2980734062cef1621419
ESF-6 <enricsofo16@gmail.com>
2024-11-16 15:06:45 +0100

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,7 +1,6 @@
 from rich import print
 from participant import load_participants
 
-<<<<<<< HEAD
 # Ruta al archivo JSON
 data_path = "data/datathon_participants.json"
 
@@ -11,6 +10,3 @@ participants = load_participants(data_path)
 # Recorrer la lista de participantes y mostrar sus nombres
 for i, participant in enumerate(participants, start=1):
     print(f"Participante {i}: {participant.name}")
-=======
-print("soy gay")
->>>>>>> 398616571ca60c8635faa0bd8c94c9e92500f751
